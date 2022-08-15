ed34fdaa35c08efb7c36d3ee194024d89cd7311c
Eliot Williams <ecwilliams@rightpoint.com>
2022-08-15 16:33:27 -0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,64 @@
+// swift-tools-version:5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RPLogging",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "RPLogging",
+            targets: [
+                "RPLogging",
+            ]
+        ),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(
+            url: "https://github.com/apple/swift-docc-plugin",
+            "1.0.0" ..< "2.0.0"
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package.
+        // A target can define a module or a test suite.
+        //
+        // Targets can depend on other targets in this package,
+        // and on products in packages which this package depends on.
+        .target(
+            name: "RPLogging",
+            dependencies: [
+            ],
+            path: "Sources/RPLogging/",
+            exclude: [
+                "Resources/README.txt",
+            ],
+            resources: [
+                .process("Resources"),
+            ]
+        ),
+        .testTarget(
+            name: "RPLoggingTests",
+            dependencies: [
+                "RPLogging",
+            ],
+            path: "Tests/RPLogging/",
+            exclude: [
+                "Resources/README.md",
+                "Toolbox/README.md",
+            ],
+            resources: [
+                .process("Resources"),
+            ]
+        ),
+    ]
+)
---
+++ Sources/RPLogging/RPLogging.docc/Resources/README.txt
@@ -0,0 +1,4 @@
+``MyLibraryName`` Documentation Resources
+============================================================
+
+Place files in this directory that are related to resources for the containing documentation catalog.
